bb42bc5b476c7d86e151eb1641a45333f3cb665b
jiuyi <jiuyi@jravity.com>
2022-05-30 15:12:18 +0800

    ts基础知识-ts概念等

---
+++ TypeScript/TS-demo/01.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description: file content
+ * @Version: file content
+ * @Autor: jiuyi
+ * @Date: 2022-05-30 10:09:31
+ * @LastEditors: jiuyi
+ * @LastEditTime: 2022-05-30 10:20:28
+ */
+let a: string = '1';
+console.log(a);
